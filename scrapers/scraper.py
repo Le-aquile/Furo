5bc4bd79c36b82bc08e6d2fdeca052ace58cd171
Mario Pisano <76888581+martino449@users.noreply.github.com>
2024-11-21 08:39:42 +0100

    Add files via upload

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape__page(url):
+def scrape__page(url, save_to_file=False):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         paragraphs = soup.find_all('p')
-        with open('output.txt', 'w', encoding='utf-8') as file:
-            for para in paragraphs:
-                file.write(para.get_text() + '\n')
+        if save_to_file:
+            with open('output.txt', 'w', encoding='utf-8') as file:
+                for para in paragraphs:
+                    file.write(para.get_text() + '\n')
+        else:
+            return [para.get_text() for para in paragraphs]
     else:
         print(f"Errore nella richiesta HTTP: {response.status_code}")
 
